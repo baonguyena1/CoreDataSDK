740e3c01a087635ade454424c50f170094ff7a8e
Bao Nguyen <baon@tech.est-rouge.com>
2020-12-24 13:59:42 +0700

    Update sample

--- CoreDataSample/ViewController.swift
+++ CoreDataSample/ViewController.swift
@@ -17,17 +17,19 @@ class ViewController: UIViewController {
         print("ViewDidLoad")
         let bundle = Bundle.main
         coreDataManager = CoreDataManager(momdName: "Sample", bundle: bundle)
-        
+    }
+
+    @IBAction private func add(_ sender: UIButton) {
         let product = Product(context: coreDataManager.context)
         product.title = "Product"
         product.subTitle = "SubTitle"
         coreDataManager.save()
-        
+    }
+    
+    @IBAction private func fetch(_ sender: UIButton) {
         let products = coreDataManager.fetchObjects(entity: Product.self)
         print(products.count)
         print(products)
     }
-
-
 }
 
